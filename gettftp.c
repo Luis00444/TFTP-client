7825a5f7ac3c7c9df1430292ee44ce7959857bae
Luis00444 <152845104+Luis00444@users.noreply.github.com>
2023-12-11 08:06:05 +0000

    argument test

--- gettftp.c
+++ gettftp.c
@@ -1,1 +1,19 @@
-//to fill
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MSG_NOT_ARGUMENTS "arguments should be 2: host file\n"
+
+int main(int argc, char* argv[]){
+    if (argc != 3){
+        write(STDOUT_FILENO,MSG_NOT_ARGUMENTS,34);
+        exit(EXIT_FAILURE);
+    }
+    
+    
+
+    exit(EXIT_SUCCESS);
+}
